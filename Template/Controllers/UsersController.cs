b1e377261e231dca5be5a6930eef92a2bef78771
marxtts@gmail.com <95075808+marxteixeira@users.noreply.github.com>
2023-03-30 11:15:53 -0300

    configurando injeção de denpendência.

---
+++ Template/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Template.Application.Interfaces;
+
+namespace Template.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService userService;
+
+        public UsersController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        public IActionResult Get()
+        {
+            this.userService.Test();
+
+            return Ok("Ok");
+        }
+    }
+}
