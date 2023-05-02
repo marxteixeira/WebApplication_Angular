a958ae8823dc9a843481ca7487123b8693b96102
marxtts@gmail.com <95075808+marxteixeira@users.noreply.github.com>
2023-05-01 22:38:36 -0300

    Rota de GetById e Find

--- Template.Application/Interfaces/IUserService.cs
+++ Template.Application/Interfaces/IUserService.cs
@@ -9,5 +9,9 @@ namespace Template.Application.Interfaces
     {
         List<UserViewModel> Get();
         bool Post(UserViewModel userViewModel);
+
+        UserViewModel GetById(string id);
+
+        bool Put(UserViewModel userViewModel);
     }
 }
